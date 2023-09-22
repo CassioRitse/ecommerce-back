8446cab2b9ba951e68488dd81f219e30e6f5d690
Cássio Ritse <cassioritse@.com>
2023-09-22 19:33:29 -0300

    feat(INDEX): to route /

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import cors from "cors";
+
 import {
   getAllProducts,
   getOneProducts,
-} from "../controllers/controllerProduct";
-import { postNewPurchase } from "../controllers/controllerPurchase";
+} from "./controllers/controllerProduct";
+
+import { postNewPurchase } from "./controllers/controllerPurchase";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", () => "server running");
 app.get("/products", getAllProducts);
 app.get("/product/:idProduct", getOneProducts);
 app.post("/purchase", postNewPurchase);
